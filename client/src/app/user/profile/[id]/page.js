aed7139127af114d5a41e336c0628c862f522b80
Ajay Korat <ajaykorat16@gmail.com>
2023-09-11 17:04:12 +0530

    Make User profile

---
+++ client/src/app/user/profile/[id]/page.js
@@ -0,0 +1,282 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+import { CForm, CCol, CFormInput, CTable, CTableHead, CTableRow, CTableHeaderCell, CTableBody, CTableDataCell } from '@coreui/react';
+import { Avatar } from 'primereact/avatar';
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
+import { useUser } from '@/lib/context/UserContext';
+import { useLeaveManagement } from '@/lib/context/LeaveManagementContext';
+import { useAuth } from '@/lib/context/AuthContext';
+import UserRoutes from '@/lib/components/Routes/UserRoutes';
+import Loader from '@/lib/components/Loader';
+import moment from 'moment';
+import toast from 'react-hot-toast';
+
+function page({ params }) {
+    const [employeeNumber, setEmployeeNumber] = useState("")
+    const [firstname, setFirstname] = useState("")
+    const [lastname, setLastname] = useState("")
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [address, setAddress] = useState("");
+    const [departments, setDepartments] = useState("");
+    const [dateOfBirth, setDateOfBirth] = useState("");
+    const [newPhoto, setNewPhoto] = useState(null);
+    const [dateOfJoining, setDateOfJoining] = useState("");
+    const [photo, setPhoto] = useState("");
+    const [carryForward, setCarryForward] = useState("")
+    const [isLoading, setIsLoading] = useState(true)
+    const [leave, setLeave] = useState([])
+    const { updateProfile, getUserProfile } = useUser()
+    const { getUserLeave } = useLeaveManagement()
+    const { auth } = useAuth();
+
+    const doj = moment(dateOfJoining).format('DD-MM-YYYY')
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let { getProfile } = await getUserProfile(params.id);
+                if (getProfile) {
+                    setEmployeeNumber(getProfile.employeeNumber)
+                    setFirstname(getProfile.firstname);
+                    setLastname(getProfile.lastname);
+                    setEmail(getProfile.email);
+                    setAddress(getProfile.address)
+                    setPhone(getProfile.phone)
+                    setDepartments(getProfile.department ? getProfile.department.name : "")
+                    setDateOfBirth(new Date(getProfile.dateOfBirth))
+                    setDateOfJoining(getProfile.dateOfJoining)
+                    setPhoto(getProfile.photo)
+                    setCarryForward(getProfile.carryForward)
+                    setIsLoading(false)
+                }
+            } catch (error) {
+                console.error("Error fetching user profile:", error);
+            }
+        };
+        fetchData();
+    }, [params.id, getUserProfile, auth]);
+
+    const fetchLeave = async () => {
+        const data = await getUserLeave()
+        setLeave(data?.leaves)
+    }
+
+    useEffect(() => {
+        fetchLeave()
+    }, [auth])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            let updateUsers = { employeeNumber, firstname, lastname, email, phone, address, dateOfBirth, dateOfJoining, photo: newPhoto || photo }
+            const data = await updateProfile(updateUsers)
+            if (data.error) {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handlePhoto = async (e) => {
+        setNewPhoto(e.target.files[0]);
+    };
+
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    return (
+        <UserRoutes>
+            {isLoading ?
+                (
+                    <Loader />
+                ) : (
+                    <>
+                        <CForm onSubmit={handleSubmit}>
+                            <div className='mainBody'>
+                                <div className='row'>
+                                    <div className='col userBlock'>
+                                        <CCol className="mb-3">
+                                            <div className="d-flex justify-content image-container mt-3">
+                                                <Avatar
+                                                    image={newPhoto ? URL.createObjectURL(newPhoto) : photo || null}
+                                                    icon={!photo && !newPhoto ? 'pi pi-user' : null}
+                                                    size={!photo && !newPhoto ? 'xlarge' : null}
+                                                    shape="circle"
+                                                    style={{
+                                                        width: '300px',
+                                                        height: '300px',
+                                                        backgroundColor: (!photo && !newPhoto) ? '#2196F3' : null,
+                                                        color: (!photo && !newPhoto) ? '#ffffff' : null
+                                                    }}
+                                                />
+                                            </div>
+                                        </CCol>
+                                        <div>
+                                            <p className='title'>PROFILE</p>
+                                        </div>
+                                        <div className="userInfo mb-3">
+                                            <div className='detail'>
+                                                <div className='row userDetail'>
+                                                    <div className='col'><strong> Employee Id </strong></div>
+                                                    <div className='col'>{employeeNumber}</div>
+                                                </div>
+                                                <div className='row userDetail'>
+                                                    <div className='col'> <strong> Name </strong> </div>
+                                                    <div className='col'>{firstname} {lastname}</div>
+                                                </div>
+                                                <div className='row userDetail'>
+                                                    <div className='col'><strong> Department </strong> </div>
+                                                    <div className='col'>{departments}</div>
+                                                </div>
+                                                <div className='row userDetail'>
+                                                    <div className='col'><strong> Email </strong> </div>
+                                                    <div className='col'>{email}</div>
+                                                </div>
+                                                <div className='row userDetail'>
+                                                    <div className='col'> <strong> Date Of Joining </strong></div>
+                                                    <div className='col'>{doj}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='col leaveTable'>
+                                        <div className='leaveHeader'>
+                                            <div>
+                                                <p className='leaveTitle'>LEAVE HISTORY</p>
+                                            </div>
+                                            <div>
+                                                <p className='carryForward'>Carry Forward: {carryForward}</p>
+                                            </div>
+                                        </div>
+                                        <CTable className='mailTable'>
+                                            <CTableHead>
+                                                <CTableRow color="dark">
+                                                    <CTableHeaderCell scope="col">Month</CTableHeaderCell>
+                                                    <CTableHeaderCell scope="col">Leave</CTableHeaderCell>
+                                                </CTableRow>
+                                            </CTableHead>
+                                            <CTableBody>
+                                                {months.map((month, index) => {
+                                                    const leaveDataForMonth = leave.find(item => {
+                                                        const date = new Date(item.monthly);
+                                                        return date.getMonth() === index;
+                                                    }) || {};
+
+                                                    return (
+                                                        <CTableRow key={index} className='tableBody'>
+                                                            <CTableDataCell>{month}</CTableDataCell>
+                                                            <CTableDataCell>{leaveDataForMonth.leave || '-'}</CTableDataCell>
+                                                        </CTableRow>
+                                                    );
+                                                })}
+                                            </CTableBody>
+                                        </CTable>
+                                    </div>
+                                </div>
+                                <div id='editUser'>
+                                    <div className='row'>
+                                        <div className='col'>
+                                            <p className='title'>EDIT USER</p>
+                                        </div>
+                                        <div className='col'>
+                                            <div className="btn-sm float-end submitButton">
+                                                <CCol>
+                                                    <Button icon="pi pi-check" type='submit' rounded aria-label="Filter" />
+                                                </CCol>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='row'>
+                                        <div className='col'>
+                                            <CCol>
+                                                <CFormInput
+                                                    id="inputFirstName"
+                                                    label="First Name"
+                                                    value={firstname}
+                                                    className='mb-3'
+                                                    onChange={(e) => setFirstname(e.target.value)}
+                                                />
+                                            </CCol>
+                                        </div>
+                                        <div className='col'>
+                                            <CCol>
+                                                <CFormInput
+                                                    id="inputLastName"
+                                                    label="Last Name"
+                                                    className='mb-3'
+                                                    value={lastname}
+                                                    onChange={(e) => setLastname(e.target.value)}
+                                                />
+                                            </CCol>
+                                        </div>
+                                    </div>
+                                    <div className='row'>
+                                        <div className='col'>
+                                            <CCol>
+                                                <CFormInput
+                                                    id="inputAddress"
+                                                    label="Address"
+                                                    placeholder="Enter your address"
+                                                    className='mb-3'
+                                                    value={address}
+                                                    onChange={(e) => setAddress(e.target.value)}
+                                                />
+                                            </CCol>
+                                        </div>
+                                        <div className='col'>
+                                            <CCol>
+                                                <CFormInput
+                                                    type="number"
+                                                    id="inputPhone"
+                                                    label="Phone Number"
+                                                    className='mb-3'
+                                                    value={phone}
+                                                    onChange={(e) => setPhone(e.target.value)}
+                                                />
+                                            </CCol>
+                                        </div>
+                                    </div>
+                                    <div className='row'>
+                                        <div className='col'>
+                                            <CCol>
+                                                <label className="form-label">Date Of Birth</label>
+                                                <Calendar
+                                                    value={dateOfBirth}
+                                                    dateFormat="dd-mm-yy"
+                                                    onChange={(e) => setDateOfBirth(e.target.value)}
+                                                    maxDate={new Date()}
+                                                    showIcon
+                                                    id="date"
+                                                    className="form-control"
+                                                />
+                                            </CCol>
+                                        </div>
+                                        <div className='col'>
+                                            <CCol>
+                                                <CFormInput
+                                                    type="file"
+                                                    className="form-control mb-3"
+                                                    label="Upload Photo"
+                                                    id="inputGroupFile04"
+                                                    accept="image/*"
+                                                    aria-describedby="inputGroupFileAddon04"
+                                                    aria-label="Upload"
+                                                    onChange={handlePhoto}
+                                                />
+                                            </CCol>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </CForm>
+                    </>
+                )}
+        </UserRoutes >
+    )
+}
+
+export default page
