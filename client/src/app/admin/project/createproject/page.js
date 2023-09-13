585f0f6e7377988e7bc1107a05affea81737ebdc
Ajay Korat <ajaykorat16@gmail.com>
2023-09-13 10:22:34 +0530

    manage dashborad

--- client/src/app/admin/project/createproject/page.js
+++ client/src/app/admin/project/createproject/page.js
@@ -38,8 +38,8 @@ const ProjectCreate = () => {
 
     const getUsers = async () => {
         if (auth?.user !== null) {
-            const { getAllUsers } = await fetchUsers();
-            setUsers(getAllUsers);
+            const data = await fetchUsers();
+            setUsers(data?.getAllUsers);
         }
     };
     useEffect(() => {
@@ -48,55 +48,55 @@ const ProjectCreate = () => {
 
     return (
         <>
-        <AdminRoutes>
-            <div className="mb-3">
-                <h2 className="mb-5 mt-2">Create Project</h2>
-            </div>
-            <CForm className="row g-3" onSubmit={handleSubmit}>
-                <CCol md={6}>
-                    <CFormInput id="inputName" label="Name" value={name} onChange={(e) => setName(e.target.value)} />
-                </CCol>
-                <CCol md={6}>
-                    <label className="form-label">Date</label>
-                    <Calendar
-                        value={startDate}
-                        dateFormat="dd-mm-yy"
-                        onChange={(e) => setStartDate(e.target.value)}
-                        maxDate={new Date()}
-                        showIcon
-                        id="date"
-                        className="form-control"
-                    />
-                </CCol>
-                <CCol xs={12}>
-                    <label htmlFor="developerSelect" className="form-label">Developers</label>
-                    <MultiSelect
-                        value={developers}
-                        onChange={(e) => setDevelopers(e.value)}
-                        options={users}
-                        size={6}
-                        style={{ border: "1px solid var(--cui-input-border-color, #b1b7c1)", borderRadius: "6px" }}
-                        optionLabel="fullName"
-                        placeholder="Select Users"
-                        id="developerSelect"
-                        className="form-control"
-                    />
-                </CCol>
-                <CCol md={12}>
-                    <label className='mb-2'>Description</label>
-                    <div className="card">
-                        <Editor
-                            value={description}
-                            onTextChange={(e) => setDescription(e.htmlValue)}
-                            className="editorContainer"
+            <AdminRoutes>
+                <div className="mb-3">
+                    <h2 className="mb-5 mt-2">Create Project</h2>
+                </div>
+                <CForm className="row g-3" onSubmit={handleSubmit}>
+                    <CCol md={6}>
+                        <CFormInput id="inputName" label="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    </CCol>
+                    <CCol md={6}>
+                        <label className="form-label">Date</label>
+                        <Calendar
+                            value={startDate}
+                            dateFormat="dd-mm-yy"
+                            onChange={(e) => setStartDate(e.target.value)}
+                            maxDate={new Date()}
+                            showIcon
+                            id="date"
+                            className="form-control"
                         />
-                    </div>
-                </CCol>
-                <CCol xs={12}>
-                    <CButton type="submit">Submit</CButton>
-                </CCol>
-            </CForm>
-         </AdminRoutes>
+                    </CCol>
+                    <CCol xs={12}>
+                        <label htmlFor="developerSelect" className="form-label">Developers</label>
+                        <MultiSelect
+                            value={developers}
+                            onChange={(e) => setDevelopers(e.value)}
+                            options={users}
+                            size={6}
+                            style={{ border: "1px solid var(--cui-input-border-color, #b1b7c1)", borderRadius: "6px" }}
+                            optionLabel="fullName"
+                            placeholder="Select Users"
+                            id="developerSelect"
+                            className="form-control"
+                        />
+                    </CCol>
+                    <CCol md={12}>
+                        <label className='mb-2'>Description</label>
+                        <div className="card">
+                            <Editor
+                                value={description}
+                                onTextChange={(e) => setDescription(e.htmlValue)}
+                                className="editorContainer"
+                            />
+                        </div>
+                    </CCol>
+                    <CCol xs={12}>
+                        <CButton type="submit">Submit</CButton>
+                    </CCol>
+                </CForm>
+            </AdminRoutes>
         </>
     );
 };
